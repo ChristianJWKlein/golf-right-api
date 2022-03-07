cf791b4fd8523f7574b4f32ed2fc639fda60715f
Christian JW Klein <cklein010715@gmail.com>
2022-03-07 16:04:59 -0500

    Separation of concerns for endpoints

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { getAllCourses } = require('./courses');
 
 //const courses = require('./courses.json');
 
@@ -8,6 +9,10 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get('/courses', getAllCourses);
+// app.get('/courses/:id', getOneCourse);
+//app.patch('/courses/:id', rateCourse)
 
 app.listen(PORT, () => {
   console.log('Listening on Port: ', PORT);
